28afdd76d74896c6d8b28489ab74e91e112c5f80
lk <likuo@richerway.cn>
2020-06-16 16:31:44 +0800

    同步代码

--- scripts/indexAsynloading.js
+++ scripts/indexAsynloading.js
@@ -5,42 +5,25 @@ var cavealoading = {
     js: false
 };
 
-//asyncLoad('link', '/static/css/main.d5c8be1b.css');
-//asyncLoad('script', script);
-/*function asyncLoad(V_tag, V_src) {
-    if (V_tag == 'script') {
-        var s = document.createElement('script');
-        s.type = 'text/javascript';
-        s.async = true;
-        s.src = V_src;
-        var x = document.getElementsByTagName('script')[0];
-        x.parentNode.insertBefore(s, x);
-        s.onload = function () {
-            cavealoading.js = true;
-        };
-    } else {
-        var s = document.createElement('link');
-        s.async = true;
-        s.rel = 'stylesheet';
-        s.href = V_src;
-        var x = document.getElementsByTagName('link')[0];
-        x.parentNode.insertBefore(s, x);
-        s.onload = function () {
-            cavealoading.css = true;
-        };
-    }
-}*/
 
 var iCount = setInterval(function () {
-    if (cavealoading.js && cavealoading.css) {
-        var deleteN = document.getElementsByTagName('body');
-        deleteN[0].removeChild(document.getElementsByTagName('canvas')[0]);
-        animationsetup = null;
-        animationresize = null;
-        draw = null;
-        clearInterval(iCount);
+    var root = document.getElementById('root');
+    if (root.style.display !== 'none') {
+        root.style.display = 'none';
     }
-}, 3500);
+    if (cavealoading.js && cavealoading.css) {
+        setTimeout(() => {
+            var deleteN = document.getElementsByTagName('body');
+            var elementsByTagName = document.getElementsByTagName('canvas');
+            deleteN[0].removeChild(elementsByTagName[0]);
+            animationsetup = null;
+            animationresize = null;
+            draw = null;
+            root.style.display = 'block';
+            clearInterval(iCount);
+        }, 5500);
+    }
+});
 
 function LoadingScript(script) {
     loadjs(script, 'foobar', {
@@ -48,7 +31,7 @@ function LoadingScript(script) {
             cavealoading.js = true;
         },
         error: function (pathsNotFound) {
-            alert('脚本加载失败！网络异常');
+
         }
     });
 }
@@ -59,21 +42,9 @@ function LoadingSheet(script) {
         var loading = loadCSS(script[i]);
         onloadCSS(loading, function () {
             load.push(true);
-        });
-    }
-    var alltrue = true;
-    if (load.length === script.length) {
-        for (var i = 0; i < load.length; i++) {
-            if (!load[i]) {
-                alltrue = false;
+            if (load.length === script.length) {
+                cavealoading.css = true;
             }
-        }
-    } else {
-        alltrue = false;
-    }
-    if (alltrue) {
-        cavealoading.js = true;
-    } else {
-        alert('样式加载失败！网络异常');
+        });
     }
 }
